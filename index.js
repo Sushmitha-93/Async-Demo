230eedf1c9d845d0133e85891e2ffc8423094ac6
Sushmitha DT <dtsushmitha@gmail.com>
2019-08-25 23:42:58 +0530

    Resolving callback hell with Promises

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 console.log("Before");
-getUser(1, user => {
-  console.log(user);
-  getRepo(user.gitHubUsername, repos => {
-    console.log(repos);
-    getCommits(repos, commits => {
-      console.log(commits);
-      //This is called callback hell - because it has nested callbacks
-    });
-  });
-});
+
+getUser(1)
+  .then(user => getRepo(user.gitHubUsername))
+  .then(repos => getCommits(repos))
+  .then(commits => console.log("Commits", commits))
+  .catch(err => console.log("Error", err.message));
 
 console.log("After");
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("Reading a user from database..");
-    callback({ id: id, gitHubUsername: "sush" });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from database..");
+      resolve({ id: id, gitHubUsername: "sush" });
+    }, 2000);
+  });
 }
 
-function getRepo(username, callback) {
-  setTimeout(() => {
-    console.log("Getting repositories..");
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getRepo(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting repositories..");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
 }
 
-function getCommits(repos, callback) {
-  setTimeout(() => {
-    console.log("Getting commits..");
-    callback([3, 4, 1]);
-  }, 1000);
+function getCommits(repos) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting commits..");
+      resolve([3, 4, 1]);
+    }, 1000);
+  });
 }
